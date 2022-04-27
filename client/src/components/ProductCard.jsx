58e6700bb9ee68d97b779ed2ebdbb77dda5bed7f
hunter10471 <rafayzia10@gmail.com>
2022-04-27 14:24:56 +0500

    Optimized product card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
+import Rating from '@mui/material/Rating';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
-export const ProductCard = ({img, discount, title, price}) => {
+
+
+
+export const ProductCard = ({img, discount, title, price, rating, shipping}) => {
   return (
-    <div className='shadow-xl w-full sm:min-w-[200px] max-w-[320px] flex-grow-1 m-4 flex-shrink-0 bg-white '>
-        <div className='relative '>
-        <img src={img} className='w-full rounded-md h-[190px] sm:h-[220px] md:h-[250px] object-cover' alt="" />
+    <div className='shadow-xl min-w-[120px] max-w-[320px] m-1 sm:m-2 md:m-4 flex-shrink-0 bg-white '>
+        <img src={img} className='rounded-sm w-[150px] h-[140px] sm:h-[190px] sm:w-[190px] md:h-[250px] md:w-[250px] object-cover' alt="" />
+        <div className='px-2 sm:px-4 md:px-6'>
+        <div className='flex mt-2 pb-2 border-b-2 flex-col'>
+            <h1 className='flex  text-secondary lg:text-2xl md:text-xl sm:text-lg text-base  '> <span className='mr-4'>{title}</span><span className=' ml-auto font-medium'>${price}</span></h1>
+            <span className='text-slate-500 text-[10px] sm:text-xs md:text-sm tracking-wide mr-auto'>Outdoor grilling stove</span>
         </div>
-        <div className='flex my-4 flex-col'>
-            <h1 className='flex  px-6 text-secondary font-medium lg:text-2xl md:text-xl sm:text-lg text-base  '> <span className='mr-4'>{title}</span><span className=' ml-auto text-primary'>${price}</span></h1>
-            <span className='text-slate-500 text-[10px] sm:text-xs md:text-sm tracking-wide px-6 mr-auto'>Outdoor grilling stove</span>
+        <div className='flex pt-2 sm:flex-row flex-col sm:items-center '>
+        <Rating name="half-rating" size='small' defaultValue={2.5} precision={0.5} readOnly />
+        <span className='ml-1 sm:ml-2 md:text-sm text-xs text-primary font-medium'>
+            2,224
+        </span>
         </div>
-        <div className='px-6 my-2'>
-            <h1 className='md:text-lg sm:text-base text-sm font-medium mb-1'>Sizes</h1>
-            <div>
-                <span className='text-xs md:text-sm text-slate-500 mr-2'>XL,</span>
-                <span className='text-xs md:text-sm text-slate-500 mr-2'>LG,</span>
-                <span className='text-xs md:text-sm text-slate-500 mr-2'>MD</span>
-            </div>
+        <span className='flex items-center my-2 md:text-sm text-xs text-slate-700'><LocalShippingIcon className='mr-1 sm:mr-2' fontSize='' /> Free shipping</span>
         </div>
-        <div className='px-6 my-2'>
-            <h1 className='md:text-lg text-base font-medium mb-1'>Colors</h1>
-            <div>
-                <input type="checkbox" name="green" className=' focus:border-green-800 cursor-pointer border-2 appearance-none rounded-[50%] mr-4 p-2 sm:p-2 md:p-3 bg-green-500 ' id="" />
-                <input type="checkbox" name="red" className='focus:border-red-800 cursor-pointer border-2 appearance-none rounded-[50%] mr-4 p-2 sm:p-2 md:p-3 bg-red-500 ' id="" />
-                <input type="checkbox" name="blue" className='focus:border-blue-800 cursor-pointer border-2 appearance-none rounded-[50%] mr-4 p-2 sm:p-2 md:p-3 bg-blue-500 ' id="" />
-                <input type="checkbox" name="black" className='focus:border-gray-800 cursor-pointer border-2 appearance-none rounded-[50%] mr-4 p-2 sm:p-2 md:p-3 bg-gray-800 ' id="" />
-            </div>
-        </div>
-        <button className='w-full font-heading border-2 bg-slate-800 hover:bg-slate-700 font-medium text-primary transition-all duration-200 py-2 md:text-base text-sm '>VIEW DETAILS</button>
+        <button className='w-full font-heading border-2 bg-slate-800 hover:bg-slate-700 font-medium text-primary transition-all duration-200 py-2 lg:text-base md:text-sm text-xs '>VIEW DETAILS</button>
     </div>
   )
 }
