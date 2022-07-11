7e73bd9b2188dfae5426978e65f8e6f542e54b07
hunter10471 <rafayzia10@gmail.com>
2022-07-11 22:21:47 +0500

    Added get route to '*' for sending build 'index.html' file

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ app.use('/api/payment', paymentRoute);
 __dirname = path.resolve();
 app.use(express.static(path.join(__dirname,'/client/build')))
 
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 4000;
 
