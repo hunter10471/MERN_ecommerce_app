3f7af024c022303bd47b2e201990477f7ef2b690
hunter10471 <rafayzia10@gmail.com>
2022-07-11 14:39:49 +0500

    Preparing for deployment

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api/orders', orderRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/payment', paymentRoute);
 
-
+//eslint-disable
 __dirname = path.resolve();
 app.use(express.static(path.join(__dirname,'/client/build')))
 
