b4ef71b2fc7c7a0ea21285aef990a42c8c1eefd4
hunter10471 <rafayzia10@gmail.com>
2022-07-12 01:40:04 +0500

    Fixed cors error [1]

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ require('./config/dbConnection');
 
 dotenv.config();
 app.use(cors());
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next(); 
+};
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
